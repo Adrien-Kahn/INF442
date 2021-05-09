97d8ecc413f0e9f46a3f64aa2c21c613ff1a7a2c
Adrien <adrien.kaet@gmail.com>
2021-05-09 14:05:39 +0200

    TD7 Done

--- TD7/code/KnnRegression.cpp
+++ TD7/code/KnnRegression.cpp
@@ -7,16 +7,56 @@
 KnnRegression::KnnRegression( int k, Dataset* dataset, int col_regr)
 : Regression(dataset, col_regr) {
 	m_k = k;
- 	// TODO Exercise 2.1
+	
+	int n = dataset->GetNbrSamples();
+	int d = dataset->GetDim();
+	
+	// Builds m_dataPts
+	m_dataPts = annAllocPts(n, d - 1);
+	
+	for (int i = 0; i < n; i++) {
+		
+		int k = 0;
+		for (int j = 0; j < d; j++) {
+			
+			if (j != col_regr) {
+				m_dataPts[i][k] = dataset->GetInstance(i)[j];
+				k++;
+			}
+			
+		}
+	
+	}
+
+	m_kdTree = new ANNkd_tree(m_dataPts, n, d - 1);
+	
 }
 
 KnnRegression::~KnnRegression() {
-	// TODO Exercise 2.1
+	annDeallocPts(m_dataPts);
+	delete m_kdTree;
 }
 
 double KnnRegression::Estimate(const Eigen::VectorXd & x) const {
 	assert(x.size()==m_dataset->GetDim()-1);
-	// TODO Exercise 2.2
+	
+	int d = m_dataset->GetDim();
+	ANNpoint queryPt = annAllocPt(d - 1);
+	for (int i = 0; i < d - 1; i++) queryPt[i] = x[i];
+	
+	ANNidxArray nnIdx = new ANNidx[m_k];
+	ANNdistArray dists = new ANNdist[m_k];
+	
+	m_kdTree->annkSearch(queryPt, m_k, nnIdx, dists);
+	
+	double yhat = 0;
+	
+	for (int i = 0; i < m_k; i++) {
+		yhat += m_dataset->GetInstance(nnIdx[i])[m_col_regr];
+	}
+	
+	return yhat / ((double) m_k);
+	
 }
 
 int KnnRegression::GetK() const {
